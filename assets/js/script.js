a8ed5d9a8ce582c2d24a17e23f137c48ad0dd351
unknown <nayefkanaan03@gmail.com>
2021-08-07 06:13:17 +0300

    assets/js/script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-var webapikey = "19966323207529dcfa55ecfd3b7c51ea";
+//"19966323207529dcfa55ecfd3b7c51ea"
+
+var city="";
+var citySearch = $("#search-city");
+var searchBtn = $("#city-search");
+var clearBtn = $("#clear-cities");
+var cityCurr = $("#city-curr");
+var tempCurr = $("#temp-curr");
+var humCurr = $("#hum-curr");
+var windCurr = $("#wind-curr");
+var uvCurr = $("#uv-curr");
+var sCity = [];
+var apiKey = "19966323207529dcfa55ecfd3b7c51ea";
